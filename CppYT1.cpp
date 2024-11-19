18089a9bf7df07abab436021dcaf80cd4c91cc51
Siddharth <sidcode25@gmail.com>
2024-11-19 17:41:20 +0530

    m

---
+++ CppYT1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+int main()
+{
+    std::cout << "Hello World" << std::endl;
+    std::cout << "Hey" << std::endl;
+    int num = 1;
+    double price = 4.55;
+    bool yes = true;
+    char grade = 'A';
+    std::string name = "Sid";
+    std::cout << "Hello " << name;
+    return 0;
+}
---
+++ rps.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <ctime>
+
+char userChoice();
+char computerChoice();
+void showChoice(char x,char y);
+void chooseWinner(char m, char n);
+
+int main(){
+    using namespace std;
+    
+    char player_1 = userChoice();
+    char player_2 = computerChoice();
+    computerChoice();
+    showChoice(player_1, player_1);
+    chooseWinner(player_1, player_2);
+    return 0;
+}
+
+char userChoice()
+{
+    char user_choice;
+    do{
+    std::cout << " Enter r, p or s";
+    std::cin >> user_choice;
+    }while(user_choice != 'r' && user_choice != 'p' && user_choice != 's');
+    return user_choice;
+}
+
+char computerChoice()
+{
+    char computer_choice;
+    srand(time(0));
+    int num = rand() % 3 + 1;
+    switch(num)
+    {
+        case 1: return 'r';
+        case 2: return 'p';
+        case 3: return 's';
+    }
+}
+
+void showChoice(char x, char y)
+{
+    std::cout << "Your choice: " << x << std::endl;
+    std::cout << "Computer choice: " << y << std::endl;
+}
+
+void chooseWinner(char m, char n)
+{
+  switch(m)
+  {
+    case 'r':
+        if(n=='r'){
+            std::cout<<"Tie";}
+        else if(n=='p'){
+            std::cout<<"Lose";}
+        else{
+            std::cout<<"Win";}
+        break;
+    case 'p':
+        if(n=='p'){
+            std::cout<<"Tie";}
+        else if(n=='s'){
+            std::cout<<"Lose";}
+        else{
+            std::cout<<"Win";}
+        break;
+    case 's':
+        if(n=='s'){
+            std::cout<<"Tie";}
+        else if(n=='r'){
+            std::cout<<"Lose";}
+        else{
+            std::cout<<"Win";}
+        break;
+  }
+}
